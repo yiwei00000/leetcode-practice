5b335a73f792b916035870c479d2a9ac1416efeb
rongjianmin <jianmin_rong@qingsongchou.com>
2019-04-09 16:22:04 +0800

    update

---
+++ src/main/java/com/rong/sort/QuickSort.java
@@ -0,0 +1,46 @@
+package com.rong.sort;
+
+/**
+ * Created by yiwei 2019-04-09
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 23, 1, 0, 9};
+        int left = 0;
+        int right = arr.length - 1;
+
+        quickSort(arr, left, right);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static void quickSort(int[] arr, int left, int right) {
+        int partitionIndex = 0;
+        if (left < right) {
+            partitionIndex = partition(arr, left, right);
+            quickSort(arr, 0, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, right);
+        }
+        swap(arr, left, partitionIndex);
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+
+        int index = left + 1;
+        for (int i = index; i < right; i++) {
+            if (arr[i] < arr[left]) {
+                swap(arr, i, index);
+                index++;
+            }
+        }
+        return index - 1;
+    }
+
+    private static void swap(int[] arr, int i, int index) {
+        int tmp = arr[i];
+        arr[i] = arr[index];
+        arr[index] = tmp;
+    }
+}
